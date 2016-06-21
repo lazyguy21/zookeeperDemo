0d0da4c75219ea23920ea66188aacecb340eaccf
lazyguy21 <15882041972@sina.cn>
2016-06-21 14:26:43 +0800

    basic api

---
+++ src/main/java/org/yyf/zookeeperDemo/HelloZookeeper.java
@@ -0,0 +1,30 @@
+package org.yyf.zookeeperDemo;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by tobi on 16-6-21.
+ */
+public class HelloZookeeper {
+    public static void main(String[] args) throws IOException {
+        String host = "10.8.12.128:32770";
+//        String host = "10.18.19.18:12181";
+        String zPath = "/";
+
+        ZooKeeper zk = new ZooKeeper(host, 15000, null);
+
+        try {
+            List<String> children = zk.getChildren(zPath, false);
+            System.out.println(children);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
