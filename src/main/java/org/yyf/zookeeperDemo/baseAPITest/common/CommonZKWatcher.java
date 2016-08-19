b8f1a10c19117ab6c8d1d9001e755c81821bede6
lazyguy21 <15882041972@sina.cn>
2016-08-19 17:31:48 +0800

    通用watcher

--- src/main/java/org/yyf/zookeeperDemo/baseAPITest/common/CommonZKWatcher.java
+++ src/main/java/org/yyf/zookeeperDemo/baseAPITest/common/CommonZKWatcher.java
@@ -1,7 +1,42 @@
 package org.yyf.zookeeperDemo.baseAPITest.common;
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by tobi on 16-8-19.
  */
-public class CommonZKWatcher {
+public class CommonZKWatcher implements Watcher {
+    private CountDownLatch countDownLatch;
+    @Override
+    public void process(WatchedEvent event) {
+        System.out.println(event);
+        Event.KeeperState state = event.getState();
+        switch (state) {
+            case Unknown:
+                break;
+            case Disconnected:
+                break;
+            case NoSyncConnected:
+                break;
+            case SyncConnected:
+                System.out.println(state);
+                countDownLatch.countDown();
+                break;
+            case AuthFailed:
+                break;
+            case ConnectedReadOnly:
+                break;
+            case SaslAuthenticated:
+                break;
+            case Expired:
+                break;
+        }
+    }
+
+    public CommonZKWatcher(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
 }
--- src/main/java/org/yyf/zookeeperDemo/baseAPITest/delete/DeleteNodeTest.java
+++ src/main/java/org/yyf/zookeeperDemo/baseAPITest/delete/DeleteNodeTest.java
@@ -1,66 +1,23 @@
 package org.yyf.zookeeperDemo.baseAPITest.delete;
 
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.yyf.zookeeperDemo.baseAPITest.common.CommonZKWatcher;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by tobi on 16-8-17.
  */
-public class DeleteNodeTest implements Watcher {
+public class DeleteNodeTest {
     private static CountDownLatch countDownLatch = new CountDownLatch(1);
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 6000, new DeleteNodeTest());
-        ZooKeeper zooKeeper2 = new ZooKeeper("localhost:2181", 6000, new DeleteNodeTest());
+        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 6000, new CommonZKWatcher(countDownLatch));
         countDownLatch.await();
 
-        try {
-//            String s = zooKeeper.create("/createPERSISTENTNodeTest", "haha".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            String s1 = zooKeeper.create("/createEPHEMERALNodeTest", "haha1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-//            String s2 = zooKeeper.create("/testC", "haha2".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            String s3 = zooKeeper.create("/testC", "haha3".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            System.out.println(s3);
-            String s4 = zooKeeper.create("/testC", "haha3".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            System.out.println(s4);
-            String s5 = zooKeeper.create("/testC", "haha3".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            System.out.println(s5);
-//            String s4 = zooKeeper.create("/createNodeTest", "haha4".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.CreateMode;
-            TimeUnit.SECONDS.sleep(1000);
-        } catch (KeeperException e) {
-            e.printStackTrace();
-        }
-    }
+//        zooKeeper.delete("/firstShot");
 
-    @Override
-    public void process(WatchedEvent event) {
-        System.out.println(event);
-        Event.KeeperState state = event.getState();
-        switch (state) {
-            case Unknown:
-                break;
-            case Disconnected:
-                break;
-            case NoSyncConnected:
-                break;
-            case SyncConnected:
-                System.out.println(state);
-                countDownLatch.countDown();
-                break;
-            case AuthFailed:
-                break;
-            case ConnectedReadOnly:
-                break;
-            case SaslAuthenticated:
-                break;
-            case Expired:
-                break;
-        }
+
     }
 }
